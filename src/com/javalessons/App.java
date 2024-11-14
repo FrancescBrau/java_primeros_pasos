e22bd6b4e7a2ff9cad5559b3cc919b0b14953b28
Francesc Brau <frjosepgbrau@gmail.com>
2024-11-14 23:46:10 +0400

    using edit change

--- src/com/javalessons/App.java
+++ src/com/javalessons/App.java
@@ -1,9 +1,11 @@
+package com.javalessons;
 import com.javalessons.model.person;
 import com.javalessons.services.IPersonaService;
 import com.javalessons.services.PersonaServiceImpl;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+        System.out.print("Hola mundo!!");
          person pers1 = new person(1, "Pepe");
         IPersonaService persService = new PersonaServiceImpl();
         persService.register(pers1); 
